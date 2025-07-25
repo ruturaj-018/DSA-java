e46b6857d7c6025045c3cd60c3928bc5a1748710
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-07-25 21:43:52 +0530

    Add comments to Linear_search.java methods
    Added descriptive comments above the print, findelement, and main methods in Linear_search.java to improve code readability and maintainability.

--- Searching/Linear_search.java
+++ Searching/Linear_search.java
@@ -1,6 +1,7 @@
 package Searching;
 public class Linear_search {
-
+    
+//Print Method
     static void print(int[] arr){
         System.out.print("Array : ");
         for(int num : arr){
@@ -8,7 +9,7 @@ public class Linear_search {
         }
         System.out.println();
     }
-
+// Find the Element in the given Array.
     static void findelement(int [] array,int num){
         int x = num;
         for(int i = 0 ; i < array.length; i++){
@@ -18,7 +19,7 @@ public class Linear_search {
             }
         }
     }
-
+// Main
     public static void main(String[] args) {
         int[] arr = {5,67,89,34,56,23,98,67,97};
         int target = 56;
