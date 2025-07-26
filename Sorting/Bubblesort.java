cd198c0730f5ba03d2b2759b7243f6a163df252a
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-07-26 23:24:19 +0530

    Added comments to the Sorting(Bubblesort)

--- Sorting/Bubblesort.java
+++ Sorting/Bubblesort.java
@@ -1,5 +1,6 @@
 package Sorting;
 public class Bubblesort {
+        //Print Method
         static void print(int[] arr){
             System.out.print("Array : ");
             for(int num : arr){
@@ -7,13 +8,13 @@ public class Bubblesort {
             }
             System.out.println();
         }
-
+        //swap Method
         static void swap(int[] arr,int start,int end ){
             int temp = arr[start];
             arr[start] = arr[end];
             arr[end] = temp;
         }
-
+        //Sorting Method
         static void bubblesort(int[] arr){
             for(int i = 0;i < arr.length;i++){
                 for(int j = 0;j < arr.length-1-i;j++){
@@ -25,8 +26,7 @@ public class Bubblesort {
             System.out.print("Sorted ");
             print(arr);
         }
-
-
+        
         public static void main(String[] args) {
             int[] array = {87,65,43,87,93,23,8,12};
             //int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
