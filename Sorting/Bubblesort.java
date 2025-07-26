462b3a489bf926d3caa9d1f6bd765cc5b45b23b4
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-07-26 23:21:29 +0530

    Updated java file

---
+++ Sorting/Bubblesort.java
@@ -0,0 +1,36 @@
+package Sorting;
+public class Bubblesort {
+        static void print(int[] arr){
+            System.out.print("Array : ");
+            for(int num : arr){
+                System.out.print(num+" ");
+            }
+            System.out.println();
+        }
+
+        static void swap(int[] arr,int start,int end ){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+        }
+
+        static void bubblesort(int[] arr){
+            for(int i = 0;i < arr.length;i++){
+                for(int j = 0;j < arr.length-1-i;j++){
+                    if(arr[j]>arr[j+1]){
+                        swap(arr,j,j+1);
+                    }
+                }
+            }
+            System.out.print("Sorted ");
+            print(arr);
+        }
+
+
+        public static void main(String[] args) {
+            int[] array = {87,65,43,87,93,23,8,12};
+            //int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            print(array);
+            bubblesort(array);
+        }
+}
