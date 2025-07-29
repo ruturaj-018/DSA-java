709e1c2287d4586252887e334de9f9788c878b46
Ruturaj Mankapure <ruturajmankapure4@gmail.com>
2025-07-29 22:46:36 +0530

    Add print and binarysearch methods to Binary_search
    Introduced a print method to display array contents and started implementing a binarysearch method. Updated main to use these methods for demonstration.

--- Searching/Binary_search.java
+++ Searching/Binary_search.java
@@ -2,5 +2,24 @@ package Searching;
 
 public class Binary_search {
 
+    static void print(int[] arr){
+        System.out.print("Array : ");
+        for(int num : arr){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+    }
 
+    static int binarysearch(int [] array,int num){
+
+        int mid = start + (end - start)/2
+    }
+
+    public static void main(String[] args) {
+        int[] arr ={78,93,24,26,45,36,15};
+        int target = 56;
+        print(arr);
+        int num = binarysearch(arr,target);
+        System.out.println(num);
+    }
 }
